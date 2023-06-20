af32b9c661b98f91984e079769c821cabf5e124e
john meraz <102620932+Johnmz20@users.noreply.github.com>
2023-06-19 20:37:22 -0400

    little update on title

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -40894,7 +40894,7 @@ const MovieView = ({ movie , onBackClick  })=>{
                         columnNumber: 17
                     }, undefined),
                     /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("span", {
-                        children: movie.Title
+                        children: movie.title
                     }, void 0, false, {
                         fileName: "src/components/movie-view/movie-view.jsx",
                         lineNumber: 12,
